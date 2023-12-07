9372bd3167b1072ffca76963efcba92a6cbeeef6
Robbie Corcoran <codewithrobbie@gmail.com>
2023-12-07 13:52:37 +0100

    Mobile first view is better fro cards. Also removed the readmore button.

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -7,13 +7,13 @@ import FeatureCard from "./FeatureCard";
 
 const Features = () => {
   return (
-    <div className="grid grid-cols-2 items-center justify-center gap-[1.5rem] bg-gradient-to-b from-spotify-black from-10% via-spotify-green to-spotify-black to-90% p-3">
+    <div className="grid md:grid-cols-2 items-center justify-center gap-[1.5rem] bg-gradient-to-b from-spotify-black from-10% via-spotify-green to-spotify-black to-90% p-3">
       <div className="grid gap-4 justify-self-end align-middle">
         <FeatureCard
           imgUrl={featuresArtistsImg}
           title={"Explore New Artists"}
           description={
-            "Explore the artists that we know you'll love, before you find them. Simply select the festival you're going to and we will recommend new and exciting artists for you based on you listening habits."
+            "Explore the artists at your upcoming festivals that we know you'll love, before you find them."
           }
         />
       </div>
