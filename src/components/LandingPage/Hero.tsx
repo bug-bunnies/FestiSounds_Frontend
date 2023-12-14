43c2f631bee361d65191e58276d13f5d2b2db0f4
Robbie Corcoran <codewithrobbie@gmail.com>
2023-12-14 15:37:38 +0100

    Updated connect with spotify button. Now, an alert will notify the user that this is currently disabled.

--- src/components/LandingPage/Hero.tsx
+++ src/components/LandingPage/Hero.tsx
@@ -1,14 +1,7 @@
 import { heroBackgroundImg } from "../../../public/assets/hero";
+import CallToAction from "./CallToAction";
 
 const Hero = () => {
-
-  const getSpotifyUserLogin = () => {
-    fetch("http://localhost:8080/api/login")
-      .then((response) => response.text())
-      .then((response) => {
-        window.location.replace(response);
-      });
-    }
 
   return (
     <section className="relative bg-hero-black bg-opacity-50 bg-center bg-no-repeat bg-blend-multiply before:absolute before:inset-0 before:bg-gradient-to-t to-10% from-spotify-black" style={{ backgroundImage: `url(${heroBackgroundImg})` }}>
@@ -19,29 +12,7 @@ const Hero = () => {
         <p className="mb-8 text-lg font-normal text-spotify-light-grey sm:px-16 lg:px-48 lg:text-xl">
           Want to get the most out of your next festival? Find new and exciting music tailored especially for you.
         </p>
-        <div className="relative flex justify-center">
-            <a
-            href="#" onClick={getSpotifyUserLogin}
-            className="inline-flex items-center justify-center rounded-full bg-spotify-green px-5 py-3 text-center text-base font-medium text-spotify-white hover:text-spotify-green hover:ring-1 hover:ring-spotify-green hover:bg-transparent focus:ring-4 focus:ring-spotify-green"
-          >
-            Connect with Spotify
-            <svg
-              className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </a>
-        </div>
+        <CallToAction />
       </div>
     </section>
   );
